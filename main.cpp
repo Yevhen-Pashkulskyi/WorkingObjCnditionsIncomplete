b02003a17d263e20b536927bed2d2369ce8fa75a
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2025-04-13 11:44:24 +0300

    version 1.0.1

--- main.cpp
+++ main.cpp
@@ -1,104 +1,117 @@
 #include <iostream>
 using namespace std;
 
-class MyClass {
-private:
+class MyClass
+{
     // Атрибути
-    int a1;
-    double a2;
-    string a3;
+    int id;
+    int age;
+    double weight;
+    string surname_name_patronymic;
 
     // Прапорці атрибутів
-    bool fa1, fa2, fa3;
+    bool fId, fAge, fWeight, fSNP;
 
     // Прапорці сетерів
-    bool fSetA1, fSetA2, fSetA3;
+    bool fSetId, fSetAge, fSetWeight, fSetSNP;
 
     // Прапорці методів
     bool fm1, fm2, fm3;
 
     // Метод контролю доступу
-    void accessControl() {
-        // Метод M1 доступний, якщо задано a2 і a3
-        if (fa2 && fa3) fm1 = true;
+    void accessControl()
+    {
+        // M1: weight && SNP
+        fm1 = (fWeight && fSNP);
 
-        // Метод M2 доступний, якщо задано a1 і a3
-        if (fa1 && fa3) fm2 = true;
+        // M2: id && SNP
+        fm2 = (fId && fSNP);
 
-        // Метод M3 доступний, якщо задано a1 і a2
-        if (fa1 && fa2) fm3 = true;
+        // M3: id && weight
+        fm3 = (fId && fWeight);
     }
 
 public:
     // Конструктор за замовчуванням
-    MyClass() {
-        // Ініціалізація прапорців
-        fa1 = fa2 = fa3 = false;
-        fSetA1 = fSetA2 = fSetA3 = true;
+    MyClass()
+    {
+        // Ініціалізація прапорців атрибутів
+        fId = fAge = fWeight = fSNP = false;
+
+        // Сеттери: id – одноразовий, інші – багаторазові
+        fSetId = true;
+        fSetAge = fSetWeight = fSetSNP = true;
+
+        // Методи
         fm1 = fm2 = fm3 = false;
     }
 
-    // Одноразові сеттери
-    void setA1(int value) {
-        if (fSetA1) {
-            a1 = value;
-            fa1 = true;
-            fSetA1 = false;
+    // Одноразовий сеттер
+    void setId(int value)
+    {
+        if (fSetId)
+        {
+            id = value;
+            fId = true;
+            fSetId = false;
             accessControl();
-        } else {
-            cout << "Сеттер A1 вже використано.\n";
+        }
+        else
+        {
+            cout << "Сеттер ID вже використано.\n";
         }
     }
 
-    void setA2(double value) {
-        if (fSetA2) {
-            a2 = value;
-            fa2 = true;
-            fSetA2 = false;
-            accessControl();
-        } else {
-            cout << "Сеттер A2 вже використано.\n";
-        }
+    // Багаторазові сеттери
+    void setAge(int value)
+    {
+        age = value;
+        fAge = true;
+        accessControl();
     }
 
-    void setA3(string value) {
-        if (fSetA3) {
-            a3 = value;
-            fa3 = true;
-            fSetA3 = false;
-            accessControl();
-        } else {
-            cout << "Сеттер A3 вже використано.\n";
-        }
+    void setWeight(double value)
+    {
+        weight = value;
+        fWeight = true;
+        accessControl();
+    }
+
+    void setSNP(string value)
+    {
+        surname_name_patronymic = value;
+        fSNP = true;
+        accessControl();
     }
 
     // Методи
-    void m1() {
-        if (fm1) {
-            cout << "Метод M1 викликано. Значення a2 = " << a2 << ", a3 = " << a3 << endl;
-        } else {
+    void m1()
+    {
+        if (fm1)
+            cout << "Метод M1 викликано. Вага = " << weight << ", ПІБ = " << surname_name_patronymic << endl;
+        else
             cout << "Метод M1 недоступний.\n";
-        }
     }
 
-    void m2() {
-        if (fm2) {
-            cout << "Метод M2 викликано. Значення a1 = " << a1 << ", a3 = " << a3 << endl;
-        } else {
+    void m2()
+    {
+        if (fm2)
+            cout << "Метод M2 викликано. ID = " << id << ", ПІБ = " << surname_name_patronymic << endl;
+        else
             cout << "Метод M2 недоступний.\n";
-        }
     }
 
-    void m3() {
-        if (fm3) {
-            cout << "Метод M3 викликано. Значення a1 = " << a1 << ", a2 = " << a2 << endl;
-        } else {
+    void m3()
+    {
+        if (fm3)
+            cout << "Метод M3 викликано. ID = " << id << ", Вага = " << weight << endl;
+        else
             cout << "Метод M3 недоступний.\n";
-        }
     }
 
     // Меню доступних методів
-    void showAvailableMethods() {
+    void showAvailableMethods()
+    {
         cout << "\nДоступні методи:\n";
         if (fm1) cout << "1. M1\n";
         if (fm2) cout << "2. M2\n";
@@ -108,64 +121,78 @@ public:
 };
 
 // ------------------- main -------------------
-int main() {
+int main()
+{
     MyClass obj;
     int choice;
 
-    while (true) {
+    while (true)
+    {
         cout << "\nМеню:\n";
-        cout << "1. Встановити A1\n";
-        cout << "2. Встановити A2\n";
-        cout << "3. Встановити A3\n";
-        cout << "4. Показати доступні методи\n";
-        cout << "5. Викликати M1\n";
-        cout << "6. Викликати M2\n";
-        cout << "7. Викликати M3\n";
+        cout << "1. Встановити ID (одноразовий)\n";
+        cout << "2. Встановити вік\n";
+        cout << "3. Встановити вагу\n";
+        cout << "4. Встановити ПІБ (Прізвище Ім'я По Батькові)\n";
+        cout << "5. Показати доступні методи\n";
+        cout << "6. Викликати M1\n";
+        cout << "7. Викликати M2\n";
+        cout << "8. Викликати M3\n";
         cout << "0. Вихід\n";
         cout << "Ваш вибір: ";
         cin >> choice;
 
-        switch (choice) {
-            case 1: {
+        switch (choice)
+        {
+        case 1:
+            {
                 int val;
-                cout << "Введіть значення для A1 (int): ";
+                cout << "Введіть ID: ";
                 cin >> val;
-                obj.setA1(val);
+                obj.setId(val);
                 break;
             }
-            case 2: {
+        case 2:
+            {
+                int val;
+                cout << "Введіть вік: ";
+                cin >> val;
+                obj.setAge(val);
+                break;
+            }
+        case 3:
+            {
                 double val;
-                cout << "Введіть значення для A2 (double): ";
+                cout << "Введіть вагу: ";
                 cin >> val;
-                obj.setA2(val);
+                obj.setWeight(val);
                 break;
             }
-            case 3: {
+        case 4:
+            {
                 string val;
-                cout << "Введіть значення для A3 (string): ";
-                cin >> val;
-                obj.setA3(val);
+                cout << "Введіть ПІБ: ";
+                cin.ignore(); // для коректного зчитування рядка після cin
+                getline(cin, val);
+                obj.setSNP(val);
                 break;
             }
-            case 4:
-                obj.showAvailableMethods();
-                break;
-            case 5:
-                obj.m1();
-                break;
-            case 6:
-                obj.m2();
-                break;
-            case 7:
-                obj.m3();
-                break;
-            case 0:
-                cout << "Завершення програми.\n";
-                return 0;
-            default:
-                cout << "Невірний вибір.\n";
+        case 5:
+            obj.showAvailableMethods();
+            break;
+        case 6:
+            obj.m1();
+            break;
+        case 7:
+            obj.m2();
+            break;
+        case 8:
+            obj.m3();
+            break;
+        case 0:
+            cout << "Завершення програми.\n";
+            return 0;
+        default:
+            cout << "Невірний вибір.\n";
         }
     }
-
-    return 0;
 }
